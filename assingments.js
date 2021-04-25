c2507121575348761244a84bc3bbf955da30acb2
Devekotment <82453103+Devekotment@users.noreply.github.com>
2021-04-25 21:52:54 +0200

    Add files via upload

---
+++ assingments.js
@@ -0,0 +1,76 @@
+/*
+const country = "Poland";
+const continent = "Europe";
+let population = 40;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+if (population > 33) {
+    console.log(`${country} population is ${population - 33} million above average.`)
+} else {
+    console.log(`${country} population is ${33 - population} milion below average.`);
+}
+
+
+
+const isIsland = "false";
+const language = "Bolivian"
+console.log(isIsland);
+console.log(language);
+
+console.log(population / 2);
+population++;
+console.log(population);
+let finlandPopulation = 6;
+console.log(population > finlandPopulation);
+let averagePopulation = 33;
+console.log(population < averagePopulation);
+const description = (country +
+    " is in " +
+    continent +
+    " and its " +
+    population +
+    " million people speak " +
+    language +
+    ". It is " +
+    isIsland +
+    " that " +
+    country +
+    " is an island.");
+console.log(description)
+
+challenge 1
+const markWeight = 78
+const johnWeight = 92
+const markHeight = 1.69
+const johnHeight = 1.95
+
+const markWeightTwo = 95
+const markHeightTwo = 1.88
+const johnWeightTwo = 85
+const johnHeightTwo = 1.76
+
+const markBmi = (markWeightTwo / markHeightTwo ** 2)
+console.log(markBmi)
+
+const johnBmi = (johnWeightTwo / johnHeightTwo ** 2)
+console.log(johnBmi)
+
+//challenge 2
+const markWeightTwo = 76
+const markHeightTwo = 1.88
+const johnWeightTwo = 85
+const johnHeightTwo = 1.76
+
+const markBmi = (markWeightTwo / markHeightTwo ** 2)
+const johnBmi = (johnWeightTwo / johnHeightTwo ** 2)
+
+if (markBmi > johnBmi) {
+    console.log(`Mark's BMI (${markBmi}) is higher than John's BMI(${johnBmi})!`)
+} else { console.log(`Mark's BMI(${markBmi}) is lower than John's BMI(${johnBmi})`) }
+
+
+const descriptionNew = `${country} is in ${continent} and its ${population} million people speak ${language}. It is ${isIsland} that ${country} is an island.`;
+console.log(descriptionNew)
+*/
---
+++ script.js
@@ -0,0 +1,141 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10)
+
+console.log('Michal');
+console.log(23);
+
+let firstName = "Michal"
+console.log(firstName);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Michal");
+
+javascriptIsFun = "TAK!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year)
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991
+
+
+// math operators
+const now = 2037
+
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 to 2 do potęgi trzeciej.
+
+const fistName = 'Jonas';
+const lastName = "Szmeterling";
+console.log(fistName + " " + lastName);
+
+// assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 15
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x)
+
+// comparison operators
+console.log(ageJonas > ageSarah) // >, <, >=, <=
+console.log(ageSarah >= 18)
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);
+
+
+const firstName = 'Michal'
+const job = "barman"
+const birthYear = 1999;
+const year = 2037
+
+const michal = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(michal);
+
+const michalNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(michalNew);
+console.log(`To jest po prostu zwyczajny string, który ma obrazować działanie tych śmiesznych kreseczek.`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String
+multiple
+lines`);
+
+
+const age = 15;
+
+if (age >= 18) {
+    console.log(`Sarah can start driving license 🚗 `)
+} else {
+    const YearsLeft = 18 - age;
+    console.log(`Sarah can't start driving license 😥. Wait another ${YearsLeft} years :)`);
+}
+let century;
+const birthYear = 1999;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century)
+
+
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear)); //string to number
+console.log(Number(inputYear) + 18);
+
+console.log(Number('czekolada')); //NaN - Not a number
+
+console.log(String(23), 23); //number to string
+
+// type coercion
+console.log('I am ' + 23 + ' years old') //number to string
+console.log('23' - '10' - 3); //string to number
+console.log('23' * "2");
+console.log('23' / "2");
+console.log("23" > '18');
+console.log('9' - '5'); // -> 4
+console.log('19' - '13' + '17'); // -> '617'
+console.log('19' - '13' + 17); // -> 23
+console.log('123' < 57); // -> false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 114
+*/
